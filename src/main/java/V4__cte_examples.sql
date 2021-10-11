d490a3d0aa1349b994e02aca37fb14c4b803f136
Artem <Artem>
2021-10-11 14:51:11 +0300

    Added a script with cte

---
+++ src/main/java/V4__cte_examples.sql
@@ -0,0 +1,16 @@
+/* Когда необходимо обозначить тему статьи перед самой статьей */
+WITH t AS (
+    UPDATE article SET text = topic_id || text
+    RETURNING *
+)
+SELECT * FROM t;
+
+
+/* Удаление статьи по какой-то теме */
+WITH article_to_delete (id) AS (
+    SELECT id, text
+    FROM article
+    WHERE text LIKE '2'
+)
+DELETE FROM article
+WHERE id in (SELECT id FROM article_to_delete)
